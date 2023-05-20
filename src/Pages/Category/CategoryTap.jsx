d4ec27bddd667efbb4b8857da7044e1f9eca056e
alam2025 <alam13100852@gmail.com>
2023-05-20 12:19:41 +0600

    categories load from server

--- src/Pages/Category/CategoryTap.jsx
+++ src/Pages/Category/CategoryTap.jsx
@@ -1,39 +1,39 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { Link } from 'react-router-dom';
 import { Tab, TabList, TabPanel, Tabs } from 'react-tabs';
 import 'react-tabs/style/react-tabs.css';
 
 const ShopByCategory = () => {
+
+      const [categories, setCategories] = useState([])
+   
+
+      useEffect(() => {
+            fetch('https://toy-car-land-server.vercel.app/categories')
+                  .then(res => res.json())
+                  .then(data => setCategories(data))
+      }, [])
       return (
             <Tabs>
                   <TabList className="flex">
-                        <Tab className="py-2 px-4 mx-2 font-semibold border-b-2 border-transparent hover:border-blue-500 cursor-pointer">
-                              Sports Car
-                        </Tab>
-                        <Tab className="py-2 px-4 mx-2 font-semibold border-b-2 border-transparent hover:border-blue-500 cursor-pointer">
-                              Truck
-                        </Tab>
-                        <Tab className="py-2 px-4 mx-2 font-semibold border-b-2 border-transparent hover:border-blue-500 cursor-pointer">
-                              Police Car
-                        </Tab>
+                        {
+                              categories.map(category => <Tab key={category._id}
+                                    className="py-2 px-4 mx-2 font-semibold border-b-2 border-transparent hover:border-blue-500 cursor-pointer">
+                                    {category.name}
+                              </Tab>)
+                        }
                   </TabList>
+                  {
+                        categories.map(category => <TabPanel key={category._id}>
+                              {category.subCategories.map(sc=><div key={sc.id}>
+                                    <Link>{sc.name}</Link>
+                              </div>)}
 
-                  <TabPanel>
-                        <div><Link>Standard Sports Car</Link></div>
-                        <div><Link>Supercar</Link></div>
-                        <div><Link>Hypercar</Link></div>
 
-                  </TabPanel>
-                  <TabPanel className=' flex flex-col gap-6'>
-                        <div><Link>Dump truck</Link></div>
-                        <div> <Link>Tanker truck</Link> </div>
-                        <div><Link>Flatbed truck</Link></div>
-                  </TabPanel>
-                  <TabPanel>
-                        <div><Link>Patrol Car</Link></div>
-                        <div> <Link>SUVs and Trucks</Link> </div>
-                        <div><Link>Pursuit cars</Link></div>
-                  </TabPanel>
+                        </TabPanel>)
+                  }
+
+                 
             </Tabs>
       );
 };
